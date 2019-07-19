457e297c9df699efa184b5ce1abff422073a5467
toor <k.nosek@ievent-management.pl>
2019-07-19 23:06:40 +0000

    new knowledge 2.0

---
+++ Sets.py
@@ -0,0 +1,9 @@
+
+# EXAMPLE OF SET
+s = set({1,2,3,4,5,6,6,6})
+print(type(s)) # <class 'set'>
+
+# IS MOST IMPORTANT !
+print(s)
+# # # RESULT # # #
+# {1, 2, 3, 4, 5, 6}
--- TuplesMethods.py
+++ TuplesMethods.py
@@ -1,7 +1,10 @@
 
 # Example of tuple
+# tuple can not be changed / modified !!!
 months = ('January', 'February', 'March', 'April', 'May', 'June', 'July',
           'August', 'September', 'October', 'November', 'December')
+
+print(type(months)) # <class 'tuple'>
 
 for month in months:
     print(month)
@@ -12,4 +15,9 @@ i = len(months) - 1
 while i >= 0 :
     print(months[i])
     i -= 1
+
+tuple_in_tuple = (1,2,3,(4,5),6,7)
+print(tuple_in_tuple)
+# ACCESS TO TUPLE
+print(tuple_in_tuple[3][1]) # 5
 
