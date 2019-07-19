942a8d46a8fe5e3a66fc89dd68e03f08c3897462
toor <k.nosek@ievent-management.pl>
2019-07-19 21:51:16 +0000

    new knowledge

---
+++ TuplesMethods.py
@@ -0,0 +1,15 @@
+
+# Example of tuple
+months = ('January', 'February', 'March', 'April', 'May', 'June', 'July',
+          'August', 'September', 'October', 'November', 'December')
+
+for month in months:
+    print(month)
+
+print('\n')
+
+i = len(months) - 1
+while i >= 0 :
+    print(months[i])
+    i -= 1
+
